fdf7fb70cb756c9e32ac7ace1a3e9f47a84231e7
larbra <108815216+larbra@users.noreply.github.com>
2025-07-02 10:52:48 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+| 
 */
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
